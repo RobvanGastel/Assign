841b5ce00c51e1019b31a701652831dac551af25
Rob <robvangastel@hotmail.com>
2017-01-02 20:37:35 +0100

    Working Hello World with Spring Boot

---
+++ assign-api/src/main/java/com/robvangastel/assign/api/Application.java
@@ -0,0 +1,18 @@
+package com.robvangastel.assign.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Rob
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ assign-api/src/main/java/com/robvangastel/assign/api/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.robvangastel.assign.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author Rob
+ */
+@RestController
+class UserController{
+ 
+//   @Autowired
+//   IFooService service;
+ 
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello world!";
+    }
+   
+//    @RequestMapping( method = RequestMethod.GET )
+//    @ResponseBody
+//    public List< Foo > findAll(){
+//       return service.findAll();
+//    }
+//   @RequestMapping( value = "/{id}", method = RequestMethod.GET )
+//   @ResponseBody
+//   public Foo findOne( @PathVariable( "id" ) Long id ){
+//      return RestPreconditions.checkFound( service.findOne( id ) );
+//   }
+// 
+//   @RequestMapping( method = RequestMethod.POST )
+//   @ResponseStatus( HttpStatus.CREATED )
+//   @ResponseBody
+//   public Long create( @RequestBody Foo resource ){
+//      Preconditions.checkNotNull( resource );
+//      return service.create( resource );
+//   }
+// 
+//   @RequestMapping( value = "/{id}", method = RequestMethod.PUT )
+//   @ResponseStatus( HttpStatus.OK )
+//   public void update( @PathVariable( "id" ) Long id, @RequestBody Foo resource ){
+//      Preconditions.checkNotNull( resource );
+//      RestPreconditions.checkNotNull( service.getById( resource.getId() ) );
+//      service.update( resource );
+//   }
+// 
+//   @RequestMapping( value = "/{id}", method = RequestMethod.DELETE )
+//   @ResponseStatus( HttpStatus.OK )
+//   public void delete( @PathVariable( "id" ) Long id ){
+//      service.deleteById( id );
+//   }
+ 
+}
