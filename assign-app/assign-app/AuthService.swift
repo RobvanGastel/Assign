50b11ec9b565aa64bd3e8d95a0cd0e7ebbbb9448
RobvanGastel <robvangastel@hotmail.com>
2017-05-19 14:29:33 +0200

    Create Bridging header for Alamofire

--- assign-app/assign-app/AuthService.swift
+++ assign-app/assign-app/AuthService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class AuthService {
     //TODO create Authenticate
---
+++ assign-app/assign-app/Bridging.h
@@ -0,0 +1,14 @@
+//
+//  Bridging.h
+//  assign-app
+//
+//  Created by Rob Van Gastel on 19/05/2017.
+//  Copyright © 2017 Assign. All rights reserved.
+//
+
+#ifndef Bridging_h
+#define Bridging_h
+
+#import <Alamofire/Alamofire-umbrella.h>
+
+#endif /* Bridging_h */
