0b594ad51f77e75c6c758f006aed784bf9d6db1b
RobvanGastel <robvangastel@hotmail.com>
2017-07-02 12:13:05 +0200

    Improve file project structure

---
+++ assign-app/assign-app/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  assign-app
+//
+//  Created by Rob Van Gastel on 02/07/2017.
+//  Copyright © 2017 Assign. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
