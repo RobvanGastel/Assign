45302fe2ba07473e121ef44b7d37abef173c925c
Rob <robvangastel@hotmail.com>
2017-07-17 21:37:18 +0200

    Add Footer

--- assign-page/src/App.vue
+++ assign-page/src/App.vue
@@ -2,6 +2,9 @@
   <div class="container-fluid">
     <navigation></navigation>
     <router-view></router-view>
+    <div class="col-xs-12 text-center">
+      <a href="#" class="button-open">Open in app</a>
+    </div>
   </div>
 </template>
 
