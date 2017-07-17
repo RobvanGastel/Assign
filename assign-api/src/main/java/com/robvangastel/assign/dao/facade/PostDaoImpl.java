084523676535d89eb355fc99a447fe1447a872f0
Rob <robvangastel@hotmail.com>
2017-07-17 20:19:05 +0200

    Add search with ignoring casing

--- assign-api/src/main/java/com/robvangastel/assign/dao/facade/PostDaoImpl.java
+++ assign-api/src/main/java/com/robvangastel/assign/dao/facade/PostDaoImpl.java
@@ -38,7 +38,7 @@ public class PostDaoImpl extends AbstractDao<Post> implements IPostDao {
     @Override
     public List<Post> findByQuery(User user, String query, int start, int size) {
         query = "%" + query + "%";
-        String queryString = "SELECT * FROM Post p JOIN User u ON p.user_id = u.id JOIN Study s ON u.study_id = s.id JOIN School sc ON sc.id = s.school_id WHERE sc.id = :school AND p.title like :query OR p.description like :query OR u.name LIKE :query ORDER BY p.dateCreated DESC";
+        String queryString = "SELECT * FROM Post p JOIN User u ON p.user_id = u.id JOIN Study s ON u.study_id = s.id JOIN School sc ON sc.id = s.school_id WHERE sc.id = :school AND LOWER(p.title) like LOWER(:query) OR LOWER(p.description) like LOWER(:query) OR LOWER(u.name) LIKE LOWER(:query) ORDER BY p.dateCreated DESC";
         return (List<Post>) entityManager.createNativeQuery(queryString, Post.class)
                 .setFirstResult(start)
                 .setMaxResults(size)
