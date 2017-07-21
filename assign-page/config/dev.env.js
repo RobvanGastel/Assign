44f43441323f364e944012c161c7cc18d751c1cf
Rob <robvangastel@hotmail.com>
2017-07-21 21:46:50 +0200

    Update favicon

--- assign-page/config/dev.env.js
+++ assign-page/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:9080/assign/api"'
+  API_URL: '"http://84.26.134.115:8080/assign/api"'
 
 })
--- assign-page/src/components/Post.vue
+++ assign-page/src/components/Post.vue
@@ -27,7 +27,7 @@ export default {
         { property: 'og:url', content: 'http://84.26.134.115:8080/assign/' },
         { property: 'og:locale', content: 'nl_NL' },
         { property: 'og:type', content: 'website' },
-        { property: 'og:image', content: '/static/share-image.jpg' },
+        { property: 'og:image', content: 'assign/resources/share-image.jpg' },
         { property: 'og:image:type', content: 'image/jpg' },
         { property: 'og:image:width', content: '1024' },
         { property: 'og:image:height', content: '512' },
@@ -37,13 +37,13 @@ export default {
         { name: 'twitter:url', content: 'http://84.26.134.115:8080/assign/' },
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:domain', content: 'assignapp.co' },
-        { name: 'twitter:image', content: '/resources/share-image.jpg' },
+        { name: 'twitter:image', content: 'assign/resources/share-image.jpg' },
         { name: 'twitter:image:alt', content: 'Help jouw medestudent' },
         { name: 'twitter:site', content: '@assignappco' },
         { name: 'twitter:creator', content: '@assignappco' }
       ],
       link: [
-        { rel: 'shortcut icon', href: '/resources/favicon.png' }
+        { rel: 'shortcut icon', href: 'assign/resources/favicon.png' }
       ]
     }
   }
