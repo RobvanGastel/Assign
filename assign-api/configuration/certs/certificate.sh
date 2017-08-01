76c0b2b822c637535031b354f0f0467070794f23
Rob van Gastel <root@149-210-180-168.colo.transip.net>
2017-08-02 00:04:28 +0200

    Merge configuration

---
+++ assign-api/configuration/certs/certificate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOMAIN="api.assignapp.nl"
+
+SERVER_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
+
+keytool -genkey -noprompt -alias server.$DOMAIN -keyalg RSA -keysize 2048 -keystore server.$DOMAIN.keystore -storepass $SERVER_PASSWORD -keypass $SERVER_PASSWORD -validity 365 -dname "C=US,ST=North Carolina,O=Red Hat,L=Raleigh,CN=server.aerogear.dev,OU=DEVELOPMENT,emailAddress=assignapp.dev@gmail.com"
+
+sed -i "s/weak42/$SERVER_PASSWORD/g" ../standalone.xml
+
+CLIENT_PASSWORD=$(head -c 2000 /dev/urandom | tr -dc a-z0-9A-Z | head -c 256)
+
+keytool -genkey -noprompt -alias client.$DOMAIN -keyalg RSA -keysize 2048 -keystore client.$DOMAIN.keystore -storepass $CLIENT_PASSWORD -keypass $CLIENT_PASSWORD -validity 365 -storetype pkcs12 -dname "C=US,ST=North Carolina,O=Red Hat,L=Raleigh,CN=client.dev.aerogear,OU=DEVELOPMENT,emailAddress=assignapp.dev@gmail.com"
+
+keytool -exportcert -keystore client.$DOMAIN.keystore -storetype pkcs12 -storepass $CLIENT_PASSWORD -alias client.$DOMAIN -keypass $CLIENT_PASSWORD -file client.$DOMAIN.cer
+keytool -import -noprompt -file client.$DOMAIN.cer -alias client.$DOMAIN -keystore client.$DOMAIN.truststore -storepass $CLIENT_PASSWORD -keypass $CLIENT_PASSWORD
