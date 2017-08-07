953e7a2de5b570f1710ec561f48b8ca20aee5e1a
Max Wammes <maxx-we@hotmail.com>
2017-08-07 23:06:20 +0200

    Added alt for the images and added a place for the headway widget

--- assign-page/components/Navigation.vue
+++ assign-page/components/Navigation.vue
@@ -9,6 +9,7 @@
 						</a>
 						<h4>Assign</h4>
 						<h5>Help jouw medestudenten</h5>
+						<div class="headway"></div>
 					</div>
 				</div>
 			</div>
--- assign-page/components/PostNavigation.vue
+++ assign-page/components/PostNavigation.vue
@@ -5,7 +5,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-12 col-md-10 col-md-offset-1">
-							<img class="logo" src="/share-logo.png">
+							<img class="logo" src="/share-logo.png" alt="Assign logo">
 							<h4>Assign</h4>
 							<h5>Help jouw medestudenten</h5>
 							<div class="button-info">Info</div>
