59c5e85fa4d453f6bb236dc69b96880149c2ad28
Rob <robvangastel@hotmail.com>
2017-08-07 15:36:49 +0200

    Update SSR test

--- assign-page/pages/_url.vue
+++ assign-page/pages/_url.vue
@@ -19,6 +19,7 @@ import PostNavigation from '../components/PostNavigation.vue'
 import NotFound from '../components/NotFound.vue'
 import Post from '../components/Post.vue'
 import axios from 'axios'
+
 export default {
   components: {
     Post,
@@ -36,6 +37,27 @@ export default {
   },
   created () {
     this.getPost(this.$route.params.url)
+  },
+  // fetch ({ store, params }) {
+  //   return axios.get('http://84.26.134.115:8080/assign/api/page/post', { params: {
+  //     url: params.url
+  //   }})
+  //   .then(function (response) {
+  //     self.post = response.data
+  //     self.profileImage = self.$apiurl + '/img/' + self.post.user.profileImage
+  //     // store.commit('setStars', res.data)
+  //   })
+  //   .catch(function (error) {
+  //     self.error = error
+  //   })
+  // }
+  asyncData ({ params }) {
+    return axios.get('http://84.26.134.115:8080/assign/api/page/post', { params: {
+      url: params.url
+    }})
+      .then((response) => {
+        return { post: response.data }
+      })
   },
   methods: {
     getPost (url) {
@@ -59,6 +81,11 @@ export default {
       link: [
         { rel: 'shortcut icon', type: 'image/png', href: '/favicon.png' }
       ]
+    }
+  },
+  computed: {
+    postId () {
+      return this.$store.state.post
     }
   }
 }
