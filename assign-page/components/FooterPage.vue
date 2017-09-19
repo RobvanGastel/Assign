37a6a66a7a9f551f661f9a8ab2f0dba797ed6ed8
Rob <robvangastel@hotmail.com>
2017-09-19 16:49:44 +0200

    Change to linking mail and twitter

--- assign-page/components/FooterPage.vue
+++ assign-page/components/FooterPage.vue
@@ -8,30 +8,30 @@
 					<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-lg-offset-1 footer-section">
 						<b>Hulp</b>
 						<ul>
-							<li><nuxt-link class="lnk" alt="Contact" :to="'/contact'">Contact</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Twitter" :to="'https://twitter.com/assignappnl'">Twitter</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Vragen" :to="'mailto:assignapp.dev@gmail.com'">Vragen</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Vragen" :to="'/faq'">Vragen</nuxt-link></li>
+							<li><nuxt-link class="lnk" alt="Contact" :to="'/#contact'">Contact</nuxt-link></li>
+							<li><a class="lnk" alt="Vragen" target="_blank" href="https://twitter.com/assignappnl">Vragen</a></li>
+							<li><a class="lnk" alt="Vragen" href="mailto:assignapp.dev@gmail.com">Vragen</a></li>
+							<li><nuxt-link class="lnk" alt="Vragen" :to="'/#faq'">Vragen</nuxt-link></li>
 						</ul>
 					</div>
 
 					<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 footer-section">
 						<b>Functies</b>
 						<ul>
-							<li><nuxt-link class="lnk" alt="Contact" :to="'/contact'">Bellen</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Twitter" :to="'https://twitter.com/assignappnl'">Appen</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Vragen" :to="'mailto:assignapp.dev@gmail.com'">SMS’n</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Vragen" :to="'/faq'">Mailen</nuxt-link></li>
+							<li><nuxt-link class="lnk" alt="Contact" :to="'/#contact'">Bellen</nuxt-link></li>
+							<li><a class="lnk" alt="Vragen" target="_blank" href="https://twitter.com/assignappnl">Appen</a></li>
+							<li><a class="lnk" alt="Vragen" href="mailto:assignapp.dev@gmail.com">SMS’n</a></li>
+							<li><nuxt-link class="lnk" alt="Vragen" :to="'/#faq'">Mailen</nuxt-link></li>
 						</ul>
 					</div>
 
 					<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 footer-section">
 						<b>Legal</b>
 						<ul>
-							<li><nuxt-link class="lnk" alt="Contact" :to="'/contact'">Voorwaarden</nuxt-link></li>
+							<li><nuxt-link class="lnk" alt="Contact" :to="'/#contact'">Voorwaarden</nuxt-link></li>
 							<li><nuxt-link class="lnk" alt="Twitter" :to="'https://twitter.com/assignappnl'">Privacy</nuxt-link></li>
 							<li><nuxt-link class="lnk" alt="Vragen" :to="'mailto:assignapp.dev@gmail.com'">Pers</nuxt-link></li>
-							<li><nuxt-link class="lnk" alt="Vragen" :to="'/faq'">Release Notes</nuxt-link></li>
+							<li><nuxt-link class="lnk" alt="Vragen" :to="'/#faq'">Release Notes</nuxt-link></li>
 						</ul>
 					</div>
 
