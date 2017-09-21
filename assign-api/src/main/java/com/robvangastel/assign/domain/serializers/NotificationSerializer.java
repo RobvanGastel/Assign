9f509245b1e8b874a1934ce927ce77e0346ea989
Rob <robvangastel@hotmail.com>
2017-09-21 20:58:29 +0200

    Update notification serializer

--- assign-api/src/main/java/com/robvangastel/assign/domain/serializers/NotificationSerializer.java
+++ assign-api/src/main/java/com/robvangastel/assign/domain/serializers/NotificationSerializer.java
@@ -1,7 +1,28 @@
 package com.robvangastel.assign.domain.serializers;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.robvangastel.assign.domain.Notification;
+
+import java.io.IOException;
+
 /**
  * Created by robvangastel on 07/09/2017.
  */
-public class NotificationSerializer {
+public class NotificationSerializer extends StdSerializer<Notification> {
+
+    public NotificationSerializer() {
+        this(null);
+    }
+
+    protected NotificationSerializer(Class<Notification> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Notification notification, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
+        jgen.writeStartObject();
+        jgen.writeEndObject();
+    }
 }
