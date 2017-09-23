7e87ed77ef497e86f1c52d5d9521936894a69552
Max Wammes <maxx-we@hotmail.com>
2017-09-23 22:23:41 +0200

    [website] Added more content (features) and basic dropdown (no style)

--- assign-page/components/MastheadNavigation.vue
+++ assign-page/components/MastheadNavigation.vue
@@ -13,13 +13,14 @@
 					<nav>
 						<ul>
 							<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Download" :to="'#appstore'">Download</nuxt-link></li>
-							<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Contact" :to="'/#contact'">Contact</nuxt-link></li>
-							<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Vragen" :to="'/#faq'">Vragen</nuxt-link></li>
+							<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Contact" :to="'/contact'">Contact</nuxt-link></li>
+							<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Vragen" :to="'/faq'">Vragen</nuxt-link></li>
 							<li class="dropdown-toggle hidden-xs hidden-sm"><nuxt-link class="lnk dropdown-toggle" title="Functies" :to="'/#functies'">Functies</nuxt-link>
-								<ul class="dropdown-menu">
-									<li><nuxt-link class="lnk lnk-dropdown" title="Functie1" :to="'/functies#functie1'">Functie1</nuxt-link></li>
-									<li><nuxt-link class="lnk lnk-dropdown" title="Functie2" :to="'/functies#functie2'">Functie2</nuxt-link></li>
-									<li><nuxt-link class="lnk lnk-dropdown" title="Functie3" :to="'/functies#functie3'">Functie3</nuxt-link></li>
+								<ul class="dropdown-nav">
+									<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Hashtags" :to="'/functies/hashtags'">Hashtags</nuxt-link></li>
+									<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Profielen" :to="'/functies/profiel'">Profielen</nuxt-link></li>
+									<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Notificaties" :to="'/functies/notificaties'">Notificaties</nuxt-link></li>
+									<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Communicatie manieren" :to="'/functies/communicatie'">Communicatie manieren</nuxt-link></li>
 								</ul>
 							</li>
 							<li><nuxt-link class="btn-navcta" title="Vraag een demo aan" :to="'#demo'">Vraag demo aan</nuxt-link></li>
--- assign-page/components/Navigation.vue
+++ assign-page/components/Navigation.vue
@@ -17,10 +17,10 @@
 								<li><nuxt-link class="lnk" title="Contact" :to="'/contact'">Contact</nuxt-link></li>
 								<li><nuxt-link class="lnk" title="Vragen" :to="'/faq'">Vragen</nuxt-link></li>
 								<li class="dropdown-toggle"><nuxt-link class="lnk dropdown-toggle" title="Functies" :to="'/functies'">Functies</nuxt-link>
-									<ul class="dropdown-menu">
-										<li><nuxt-link class="lnk lnk-dropdown" title="Functie1" :to="'/functies#functie1'">Functie1</nuxt-link></li>
-										<li><nuxt-link class="lnk lnk-dropdown" title="Functie2" :to="'/functies#functie2'">Functie2</nuxt-link></li>
-										<li><nuxt-link class="lnk lnk-dropdown" title="Functie3" :to="'/functies#functie3'">Functie3</nuxt-link></li>
+									<ul class="dropdown-nav">
+										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Hashtags" :to="'/functies/hashtags'">Hashtags</nuxt-link></li>
+										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Profielen" :to="'/functies/profiel'">Profielen</nuxt-link></li>
+										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Notificaties" :to="'/functies/notificaties'">Notificaties</nuxt-link></li>
 									</ul>
 								</li>
 								<li><nuxt-link class="btn-navcta" title="Vraag een demo aan" :to="'#demo'">Vraag demo aan</nuxt-link></li>
