21a17d667b280edc7717b10e4788e23376e3d632
Max Wammes <maxx-we@hotmail.com>
2017-09-24 21:44:43 +0200

    [website] Added the basic contact page and changed some links/text

---
+++ assign-page/components/MastheadSub.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="row component-masthead-sub">
+    <section class="masthead-sub section-padding">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-10 col-lg-8 col-lg-offset-1">
+            <h1>Kom in contact</h1>
+            <h3>Wij beantwoorden graag jouw vragen, je kunt op verschillende manieren met ons in contact komen.</h3>
+						<a class="btn btn-primary" title="Twitter" target="_blank" href="https://twitter.com/assignappnl">Bekijk ons Twitter</a>
+						<a class="btn btn-primary" title="Mail" href="mailto:assignapp%2edev%40gmail%2ecom">Mail ons</a>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>
--- assign-page/components/Navigation.vue
+++ assign-page/components/Navigation.vue
@@ -3,24 +3,24 @@
 		<section class="brand-nav">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12 col-md-10 col-md-offset-1">
+					<div class="col-xs-12 col-lg-10 col-lg-offset-1">
 
 						<nuxt-link :to="'/'" class="logo" title="Ga naar home">
 							<img class="logo-image" alt="Assign logo" src="/logo.png">
 							<h4 class="logo-text">Assign</h4>
 						</nuxt-link>
-						<div class="headway"></div>
 
 						<nav>
 							<ul>
-								<li><nuxt-link class="lnk" title="Download" :to="'#appstore'">Download</nuxt-link></li>
-								<li><nuxt-link class="lnk" title="Contact" :to="'/contact'">Contact</nuxt-link></li>
-								<li><nuxt-link class="lnk" title="Vragen" :to="'/faq'">Vragen</nuxt-link></li>
-								<li class="dropdown-toggle"><nuxt-link class="lnk dropdown-toggle" title="Functies" :to="'/functies'">Functies</nuxt-link>
+								<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Download" :to="'#appstore'">Download</nuxt-link></li>
+								<li><nuxt-link class="lnk hidden-xs hidden-sm" title="Contact" :to="'/contact'">Contact</nuxt-link></li>
+								<!-- <li><nuxt-link class="lnk hidden-xs hidden-sm" title="Vragen" :to="'/faq'">Vragen</nuxt-link></li> -->
+								<li class="dropdown-toggle hidden-xs hidden-sm"><nuxt-link class="lnk dropdown-toggle" title="Functies" :to="'/#functies'">Functies</nuxt-link>
 									<ul class="dropdown-nav">
 										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Hashtags" :to="'/functies/hashtags'">Hashtags</nuxt-link></li>
 										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Profielen" :to="'/functies/profiel'">Profielen</nuxt-link></li>
 										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Notificaties" :to="'/functies/notificaties'">Notificaties</nuxt-link></li>
+										<li><nuxt-link class="lnk lnk-dropdown" title="Lees over Communicatie manieren" :to="'/functies/communicatie'">Communicatie manieren</nuxt-link></li>
 									</ul>
 								</li>
 								<li><nuxt-link class="btn-navcta" title="Vraag een demo aan" :to="'#demo'">Vraag demo aan</nuxt-link></li>
