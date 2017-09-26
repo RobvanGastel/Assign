52be576756465bdbea449fe28b010aa59befc6ff
Max Wammes <maxx-we@hotmail.com>
2017-09-26 22:45:05 +0200

    [website] changed the setps cta's to links

--- assign-page/components/Features.vue
+++ assign-page/components/Features.vue
@@ -13,16 +13,16 @@
 
 						<div class="col-xs-12 col-sm-6">
 							<ul>
-								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Plaats een vraag op het overzicht van jouw school met <nuxt-link class="lnk lnk-primary" title="Lees over Hashtags" :to="'/functies/hashtags'">#hashtags</nuxt-link> om gericht hulp te vragen</p></li>
-								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Krijg <nuxt-link class="lnk lnk-primary" title="Lees over Notificaties" :to="'/functies/notificaties'">notificaties</nuxt-link> wanneer iemand jou wilt helpen</p></li>
+								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Plaats een vraag op het overzicht van jouw school met <nuxt-link class="lnk lnk-secondary" title="Lees over Hashtags" :to="'/functies/hashtags'">#hashtags</nuxt-link> om gericht hulp te vragen</p></li>
+								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Krijg <nuxt-link class="lnk lnk-secondary" title="Lees over Notificaties" :to="'/functies/notificaties'">notificaties</nuxt-link> wanneer iemand jou wilt helpen</p></li>
 								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Zoek gestelde vragen op specifieke woorden</p></li>
 							</ul>
 						</div>
 
 						<div class="col-xs-12 col-sm-6">
 							<ul>
-								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Het bekijken van een student zijn/haar vragen en activiteit kan bij hun <nuxt-link class="lnk lnk-primary" title="Lees over Profielen" :to="'/functies/profiel'">profiel</nuxt-link></p></li>
-								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Stel <nuxt-link class="lnk lnk-primary" title="Lees over Communicatie manieren" :to="'/functies/communicatie'">manieren</nuxt-link> in om in contact te komen via het profiel</p></li>
+								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Het bekijken van een student zijn/haar vragen en activiteit kan bij hun <nuxt-link class="lnk lnk-secondary" title="Lees over Profielen" :to="'/functies/profiel'">profiel</nuxt-link></p></li>
+								<li><img src="../assets/img/checkmark.svg" alt="Assign feature checkmark" class="features-checkmark"><p>Stel <nuxt-link class="lnk lnk-secondary" title="Lees over Communicatie manieren" :to="'/functies/communicatie'">manieren</nuxt-link> in om in contact te komen via het profiel</p></li>
 							</ul>
 	          </div>
 
--- assign-page/components/Steps.vue
+++ assign-page/components/Steps.vue
@@ -9,11 +9,14 @@
 						<img src="../assets/img/illustration-problem.svg" alt="Assign chat function" class="step-illustration" draggable="false">
 					</div>
 
-          <div class="col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1">
+          <div class="step-text col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1">
             <h5>Vastlopen</h5>
             <h2>Het lukt niet altijd alleen</h2>
             <p>Iedereen loopt wel eens vast bij een probleem, dit is jammer wanneer een ander de oplossing kan uitleggen waardoor jij weer verder kunt.</p>
-            <nuxt-link class="btn btn-primary" title="Lees meer over vastlopen" :to="'/blog/vastlopen-kan-gebeuren'">Lees meer over vastlopen</nuxt-link>
+            <nuxt-link class="lnk lnk-primary" title="Lees meer over vastlopen" :to="'/blog/vastlopen-kan-gebeuren'">
+							Lees meer over vastlopen
+							<img src="../assets/img/icon-cta-arrow.svg" alt="Lees meer" class="cta-arrow" draggable="false">
+						</nuxt-link>
           </div>
 
         </div>
@@ -28,11 +31,14 @@
 						<img src="../assets/img/illustration-chat.svg" alt="Assign chat function" class="step-illustration" draggable="false">
 					</div>
 
-					<div class="col-xs-12 col-sm-6 col-lg-5 pull-right">
+					<div class="step-text col-xs-12 col-sm-6 col-lg-5 pull-right">
             <h5>Helpen</h5>
             <h2>Elkaar helpen kan op talloze manieren</h2>
             <p>Een groot probleem kan veel aandacht vragen terwijl een korte vragen juist met een enkel berichtje opgelost kan worden.</p>
-            <nuxt-link class="btn btn-primary" title="Bekijk de communicatie mogelijkheden" :to="'/functies/communicatie'">Bekijk de manieren</nuxt-link>
+						<nuxt-link class="lnk lnk-primary" title="Bekijk de communicatie mogelijkheden" :to="'/functies/communicatie'">
+							Bekijk de manieren
+							<img src="../assets/img/icon-cta-arrow.svg" alt="Lees meer" class="cta-arrow" draggable="false">
+						</nuxt-link>
           </div>
 
         </div>
@@ -47,11 +53,14 @@
 						<img src="../assets/img/illustration-notification.svg" alt="Assign chat function" class="step-illustration" draggable="false">
 					</div>
 
-          <div class="col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1">
+          <div class="step-text col-xs-12 col-sm-6 col-lg-5 col-lg-offset-1">
             <h5>Voor elk student</h5>
             <h2>Iedereen kan helpen</h2>
             <p>Elk student heeft unieke eigenschappen en kennis die een ander niet heeft en andersom ook. Door Assign kunnen studenten elkaar gemakkelijk zelfstandig helpen.</p>
-            <nuxt-link class="btn btn-primary" title="Lees over coöperatief leren" :to="'/blog/cooperatief-leren'">Lees over coöperatief leren</nuxt-link>
+						<nuxt-link class="lnk lnk-primary" title="Lees over coöperatief leren" :to="'/blog/cooperatief-leren'">
+							Lees over coöperatief leren
+							<img src="../assets/img/icon-cta-arrow.svg" alt="Lees meer" class="cta-arrow" draggable="false">
+						</nuxt-link>
           </div>
 
         </div>
