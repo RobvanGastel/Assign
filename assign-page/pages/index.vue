ade55ff8720774215e68c6f54b84e66100c70b94
Rob <robvangastel@hotmail.com>
2017-10-03 23:49:32 +0200

    Possible fix for counter

--- assign-page/pages/index.vue
+++ assign-page/pages/index.vue
@@ -7,7 +7,7 @@
     <banner></banner>
     <modal-license></modal-license>
     <div>
-		  <button v-on:click="counter += 1">Add 1</button>
+		  <button v-on:click="incrementCounter">Add 1</button>
 		  <p>The button above has been clicked {{ counter }} times.</p>
 		</div>
     <footer-page></footer-page>
@@ -47,6 +47,11 @@ export default {
       //   selector: '.headway-widget',
       //   account: 'J5Kp6x'
       // }
+    }
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
     }
   },
   // created () {
