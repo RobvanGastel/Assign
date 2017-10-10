8e0fe95b66008a6b03a0762259245ac44bed62e3
Max Wammes <maxx-we@hotmail.com>
2017-10-10 15:21:11 +0200

    [website] Made some website changes, illustrations, text etc.

---
+++ assign-page/components/MastheadSub404.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="row component-masthead-sub">
+    <section class="masthead-sub section-padding">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-5 pull-right">
+						<img src="../assets/img/illustration-404.svg" alt="Assign niet gevonden" class="step-illustration" draggable="false">
+					</div>
+
+					<div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1 content-text">
+            <h1>Deze pagina is niet gevonden.</h1>
+            <h3>Kom met ons in contact zodat wij dit kunnen oplossen.</h3>
+						<nuxt-link class="btn btn-primary" title="Contact" :to="'/contact'">Ga naar Contact</nuxt-link>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>
