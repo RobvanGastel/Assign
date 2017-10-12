bdf0ae3a58d69cdccbdc3a49357314545cd0a079
Rob <robvangastel@hotmail.com>
2017-10-12 15:07:25 +0200

    Update modal implementation

--- assign-page/components/mast/Masthead.vue
+++ assign-page/components/mast/Masthead.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="row component-masthead">
+
     <masthead-navigation></masthead-navigation>
     <section class="masthead section-padding">
 			<div class="container">
@@ -11,7 +12,7 @@
           <div class="col-md-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 text-center">
             <h3>Assign helpt bij het creëren van een communicatieve leeromgeving waar studenten zelfstandig en samenwerkend leren doormiddel van een iOS App.</h3>
             <button class="btn btn-primary" title="Vraag een demo versie aan" @click="showModal = true">Vraag een demo versie aan</button>
-						{{ showModal }}
+            <modal-banner v-if="showModal" @close="showModal = false"></modal-banner>
             <nuxt-link class="btn btn-secundary hidden-xs" title="Deel met jouw school" :to="'#delen'">Deel met jouw school</nuxt-link>
             <h4>Licht jouw school in door dit te delen.</h4>
           </div>
@@ -25,9 +26,11 @@
 
 <script>
 import MastheadNavigation from './MastheadNavigation'
+import ModalBanner from '../modal/ModalBanner'
 export default {
   components: {
-    MastheadNavigation
+    MastheadNavigation,
+    ModalBanner
   },
   data: () => ({
     showModal: false
