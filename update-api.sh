8bbc6609b6724268f9ee7a394889c5d75cad8a42
Rob <robvangastel@hotmail.com>
2017-10-17 13:39:31 +0200

    Update api command

--- update-api.sh
+++ update-api.sh
@@ -7,4 +7,15 @@ sudo git pull
 
 cd ./assign-api/
 
+# Build the maven package
+sudo mvn package
 
+sudo cp -r /opt/assign-app/assign-api/target/assign-api-1.0-SNAPSHOT.war /opt/assign-app/assign-api/configuration
+
+cd configuration
+
+sudo mv assign-api-1.0-SNAPSHOT.war assign-api.war
+
+cd ..
+
+docker build -t wildfly .
