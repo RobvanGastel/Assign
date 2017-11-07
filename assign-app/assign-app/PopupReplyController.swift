9ad97cadd57af5880bdd8b90a48d44d49b967ec6
Rob <robvangastel@hotmail.com>
2017-11-07 21:38:03 +0100

    Update to xib's for reply popup

---
+++ assign-app/assign-app/PopupReplyController.swift
@@ -0,0 +1,13 @@
+//
+//  PopupReplyController.swift
+//  assign-app
+//
+//  Created by Rob Van Gastel on 07/11/2017.
+//  Copyright © 2017 Assign. All rights reserved.
+//
+
+import Foundation
+
+class PopupReplyController {
+    
+}
