ce799b95f6d0eecb6510affc176a6065c5d4671b
Rob <robvangastel@hotmail.com>
2017-12-02 00:10:25 +0100

    Remove merge conflict

--- assign-dashboard/pages/schools/index.vue
+++ assign-dashboard/pages/schools/index.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div id="schools">
     <v-flex xs12 sm6 md3>
@@ -28,84 +27,13 @@
         </v-card-text>
         <v-card-actions>
           <div style="float:right !important">
-
-          <v-btn primary flat router nuxt>Add school</v-btn>
-                    </div>
+            <v-btn primary flat router nuxt>Add school</v-btn>
+          </div>
         </v-card-actions>
       </v-card>
-=======
-<template>  
-  <v-layout row justify-center>
-    <v-flex xs11 sm11 md11>
-      <div id="schools">
-        <v-flex xs12 sm6 md12>
-          <v-card>
-            <v-card-text>
-            <h6>Create school</h6>
-              <v-container fluid>
-                <v-flex>
-                  <v-layout row>
-                    <v-flex xs6>
-                      <v-text-field
-                        name="schoolname"
-                        label="School name"
-                        id="schoolname"
-                        v-model="schoolname"
-                        counter
-                        max="15"
-                      ></v-text-field>
-                    </v-flex>
-                    <v-spacer></v-spacer>
-                    <v-flex xs5>
-                      <v-text-field
-                        name="amount"
-                        label="Amount of students"
-                        id="students"
-                      ></v-text-field>
-                    </v-flex>
-                  </v-layout>
-                  <div class="float-right">
-                    <v-btn class="btn-no-margin" primary flat router nuxt>Add school</v-btn>
-                  </div>
-                </v-flex>
-              </v-container>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-        <v-card>
-          <v-card-title>
-            <h6>Schools</h6>
-            <v-spacer></v-spacer>
-            <v-text-field
-              append-icon="search"
-              label="Search"
-              single-line
-              hide-details
-              v-model="search"
-            ></v-text-field>
-          </v-card-title>
-          <v-data-table
-              v-bind:headers="headers"
-              v-bind:search="search"
-              v-bind:pagination.sync="pagination"
-              :total-items="totalSchools"
-              :loading="loading"
-              :items="schools"
-              hide-actions
-              class="elevation-1"
-            >
-            <template slot="items" scope="props">
-              <td>{{ props.item.id }}</td>
-              <td class="text-xs-right">{{ props.item.name }}</td>
-              <td class="text-xs-right">{{ props.item.schoolCode }}</td>
-              <td class="text-xs-right">{{ props.item.DateCreated }}</td>
-            </template>
-          </v-data-table>
-        </v-card>
-      </div>
->>>>>>> origin/feature_dashboard
     </v-flex>
   </v-layout>
+</div>
 </template>
 
 <script>
@@ -174,9 +102,8 @@ export default {
 #schools .divider {
   background-color: rgba(255, 255, 255, 0.8);
 }
-<<<<<<< HEAD
+
 </style>
-=======
 
 .float-right {
   float: right;
@@ -186,4 +113,3 @@ export default {
   margin: 0px;
 }
 </style>
->>>>>>> origin/feature_dashboard
