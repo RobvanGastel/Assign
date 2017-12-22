9d2459b967224b990ebad651ccf259fd21f45123
Rob <robvangastel@hotmail.com>
2017-12-23 00:36:39 +0100

    Update deployment method

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# SSH to server
+# TODO: Fix only works on Linux or MacOS!
+ssh rob@***REMOVED*** -p 2122 << EOF
+
 cd /opt/assign-app
 
 # Pull the new changes
 sudo git pull
 
+# Update API
 cd ./assign-api/
 
 # Build the maven package
@@ -24,4 +29,14 @@ docker build -t wildfly .
 
 cd ..
 
+# Update website
+cd ./assign-page/
+
+# Update docker with the new build
+docker build -t assign-page .
+
+cd ..
+
 docker-compose up -d
+
+EOF
